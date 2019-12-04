8d8bed8aa91ad583563392e5413a166b97dd8b1f
juanFds <juan9794@gmail.com>
2019-12-03 22:59:34 -0300

    Deploy (?

---
+++ pokemon-exp-candy/precache-manifest.a3a64c6219b55d07e8c5939c88f5f1f6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd8e0a01056cb74ec82459b072238ba",
+    "url": "/pokemon-exp-candy/assets/exp-candy-l.png"
+  },
+  {
+    "revision": "97b886cd53fda2b01ae2d445ad417f62",
+    "url": "/pokemon-exp-candy/assets/exp-candy-m.png"
+  },
+  {
+    "revision": "1a13b5f33cbd3e68c5832187c2276190",
+    "url": "/pokemon-exp-candy/assets/exp-candy-s.png"
+  },
+  {
+    "revision": "206a99b7379dabd6cb7f1155ee39c6a1",
+    "url": "/pokemon-exp-candy/assets/exp-candy-xl.png"
+  },
+  {
+    "revision": "6320437ed3b425c104e3fd7472a7ffe6",
+    "url": "/pokemon-exp-candy/assets/exp-candy-xs.png"
+  },
+  {
+    "revision": "4b45fc4ef00851a028427cf3b7f19fa4",
+    "url": "/pokemon-exp-candy/assets/missignNo.png"
+  },
+  {
+    "revision": "739082ebd40fafc18d4e6e4442708a0d",
+    "url": "/pokemon-exp-candy/assets/pokeball.png"
+  },
+  {
+    "revision": "7c77b8d66df88f8f0c24f75facbd2868",
+    "url": "/pokemon-exp-candy/favicon.ico"
+  },
+  {
+    "revision": "54967496d8595df1b0813259a8a8aa47",
+    "url": "/pokemon-exp-candy/index.html"
+  },
+  {
+    "revision": "cfaa2f79a8ce5bdbb0a41cd164d23452",
+    "url": "/pokemon-exp-candy/manifest.json"
+  },
+  {
+    "revision": "6fc50e908a601ed9dfa8",
+    "url": "/pokemon-exp-candy/static/css/main.58761874.chunk.css"
+  },
+  {
+    "revision": "6fc50e908a601ed9dfa8",
+    "url": "/pokemon-exp-candy/static/js/main.6fc50e90.chunk.js"
+  },
+  {
+    "revision": "9c4c61ee43acb2d6d55e",
+    "url": "/pokemon-exp-candy/static/js/runtime~main.9c4c61ee.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/pokemon-exp-candy/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
